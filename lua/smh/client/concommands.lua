0e884daee303dbf097d81bbced0031a7d93f3221
masterprophet117 <masterprophet117@gmail.com>
2024-11-16 23:09:57 -0800

    Update concommands.lua

--- lua/smh/client/concommands.lua
+++ lua/smh/client/concommands.lua
@@ -10,6 +10,14 @@ concommand.Add("smh_record", function()
     SMH.Controller.Record()
 end)
 
+concommand.Add("smh_delete", function()
+	local frame = SMH.State.Frame
+	local ids = SMH.UI.GetKeyframesOnFrame(frame)
+	if not ids then return end
+
+    SMH.Controller.DeleteKeyframe(ids)
+end)
+
 concommand.Add("smh_next", function()
     local pos = SMH.State.Frame + 1
     if pos >= SMH.State.PlaybackLength then
@@ -67,6 +75,102 @@ concommand.Add("smh_makescreenshot", function(pl, cmd, args)
         print("Specified starting frame is outside of the current Frame Count!")
     end
 end)
+
+concommand.Add("smh_smoothone", function()
+    local tab = {}
+    table.insert(tab,"Record")
+    table.insert(tab,"Next")
+    table.insert(tab,"Record")
+    table.insert(tab,"Prev")
+    table.insert(tab,"Prev")
+    table.insert(tab,"Record")
+    table.insert(tab,"Next")
+    table.insert(tab,"Remove")
+    SmoothRunComms(tab)
+
+end)
+
+
+concommand.Add("smh_smooth", function(ply,cmd,args)
+    local tab = {}
+    table.insert(tab,"Record")
+    table.insert(tab,"Next")
+    table.insert(tab,"Record")
+    table.insert(tab,"Prev")
+    table.insert(tab,"Prev")
+    table.insert(tab,"Record")
+    table.insert(tab,"Next")
+    table.insert(tab,"Remove")
+    table.insert(tab,"Next")
+    table.insert(tab,"Next")
+    table.insert(tab,"Record")
+    table.insert(tab,"Prev")
+    table.insert(tab,"Prev")
+    table.insert(tab,"Record")
+    table.insert(tab,"Prev")
+    table.insert(tab,"Prev")
+    table.insert(tab,"Record")
+    table.insert(tab,"Next")
+    table.insert(tab,"Remove")
+
+    local j = 1
+    local k = 3
+    for i=1,args[1] do
+        for i=1,j do
+        table.insert(tab,"Next")
+        table.insert(tab,"Next")
+        table.insert(tab,"Remove")
+        end
+        table.insert(tab,"Next")
+        table.insert(tab,"Next")
+        table.insert(tab,"Record")
+        for i=1,k do
+        table.insert(tab,"Prev")
+        table.insert(tab,"Prev")
+        table.insert(tab,"Record")
+        end
+        table.insert(tab,"Next")
+        table.insert(tab,"Remove")
+        
+        j = j +1
+        k = k +1
+    end
+    for i=1,j do
+        table.insert(tab,"Next")
+        table.insert(tab,"Next")
+        table.insert(tab,"Remove")
+    end
+    table.insert(tab,"Next")
+
+    SmoothRunComms(tab)
+
+end);
+
+function Remove()
+    RunConsoleCommand("smh_delete","");
+end
+function Next()
+    RunConsoleCommand("smh_next","")
+end
+function Prev()
+    RunConsoleCommand("smh_previous","")
+end
+function Record()
+    RunConsoleCommand("smh_record","")
+end
+
+function SmoothRunComms(tab)
+    local timestart = 0.01
+    local timeinterval = 0.01
+    for key,comm in ipairs(tab) do
+        timer.Simple(timestart, function() 
+            _G[comm]()
+        end
+        )
+        timestart = timestart + timeinterval
+    end
+    
+end
 
 CreateClientConVar("smh_startatone", 0, true, false, nil, 0, 1)
 CreateClientConVar("smh_currentpreset", "default", true, false)
